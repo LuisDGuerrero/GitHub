60f7006f7128ab511bdbbba47cea9b426f6a1b60
LuisDGuerrero <emojila0@gmail.com>
2025-02-13 15:23:41 +0000

    Actualizacion3

--- src/githab/GitHab.java
+++ src/githab/GitHab.java
@@ -6,6 +6,7 @@ public class GitHab {
     public static void main(String[] args) {
         System.out.println("repositorio git");
         System.out.println("hola mundo");
+        System.err.println("Cambio echo desde GitHub");
     }
     
 }
